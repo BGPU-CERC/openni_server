376616a6fb57d696be7eaf904158c1e72e4c356f
readandwrite864 <mailto:63188034+readandwrite864@users.noreply.github.com>
2023-12-21 11:52:11 +0900

    add: version

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 
 int main(int argc, char *argv[])
 {
-  Array<DeviceInfo> deviceInfoList;
   Status status;
+
+  Version version = OpenNI::getVersion();
+  printf("OpenNI version: %d.%d.%d, build: %d\n",
+         version.major, version.minor, version.maintenance, version.build);
 
   if (status = error(OpenNI::initialize()))
     return status;
 
+  Array<DeviceInfo> deviceInfoList;
   OpenNI::enumerateDevices(&deviceInfoList);
-
   for (int i = 0; i < deviceInfoList.getSize(); i++)
   {
     DeviceInfo deviceInfo = deviceInfoList[i];
-    printf("Device: %s %s, URI: %s, VID: %d, PID: %d, SN: %s",
+    printf("Device: %s %s, URI: %s, VID: %d, PID: %d, SN: %s\n",
            deviceInfo.getVendor(), deviceInfo.getName(), deviceInfo.getUri(),
            deviceInfo.getUsbVendorId(), deviceInfo.getUsbProductId(),
            deviceInfo.getSerialNumber());
